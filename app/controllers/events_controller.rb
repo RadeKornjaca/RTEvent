a77aeb56218d947745f984fe0d2db9745c69ac4f
kornjacaRade <radisic.rade@gmail.com>
2015-08-13 12:07:44 +0200

    Solved problem with user adding an event through event controller. No more hidden field in New Event form.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -27,7 +27,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
@@ -72,6 +72,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :starts_at, :place_id, :description, :user_id)
+      params.require(:event).permit(:title, :starts_at, :place_id, :description)
     end
 end
